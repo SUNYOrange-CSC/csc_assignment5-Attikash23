aa88ec7612b8b680f6ab008aafeba5b7377669ae
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-05-05 13:45:42 -0400

    Add files via upload

---
+++ Ch5_file_game.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+/*int val, product;
+cout << "Enter a number: ";
+cin >> val;
+product = val * 10;
+while (product < 100)
+{
+  cout << "Enter a number: ";
+  cin >> val;
+  product = val * 10;
+  cout << product << endl;
+}*/
+  
+ /*int numm;
+ int a = 10;
+  for( numm = 0; numm <= 1000; numm += 10)
+cout << numm << endl;*/
+
+ /* int count;
+  for (count = 0; count < 50; count++)
+    cout << "count is " << count << endl;*/
+
+/*int x = 50;
+while (x > 0)
+{
+--x;
+cout << x << " seconds to go\n";
+}*/
+
+ /* int num, bigNum, power,count;
+  cout << "enter an integer ";
+  cin >> num;
+  cout << "what power do you want it raised to ?\n ";
+  cin >> power;
+  bigNum = num;
+  while (count++ < power);
+  bigNum *= num;
+  cout << "the result is "<< bigNum<< endl;*/
+
+  /*int speed, hours_traveled, distance = 0;
+
+    cout << "What is your speed? ";
+    while(!(cin >> speed) || (speed < 0))
+    {
+        cout << "ERROR: speed must be greater than 0 \n" << endl;
+    }
+
+    cout << "What is the hours traveled? ";
+    while(!(cin >> hours_traveled) || (hours_traveled < 1))
+    {
+        cout << "ERROR: number of hours traveled must be greater than 1 \n"<< endl;    
+    }
+
+    cout << "Hour   Distance Traveled" << endl;
+    for(int i = 0; i < hours_traveled; i++)
+    {
+        distance += speed;
+        cout << "   " << (i + 1) << "   " 
+                << distance 
+                << endl;
+    }*/
+
+  /*const int INCREMENTAL = 2;
+    int num_of_days;
+    float penny = .01, total = 0;
+
+    cout << "number of days: ";
+    while(!(cin >> num_of_days) || (num_of_days < 1))
+    {
+        cout << " need at least 1 day"<< endl;
+       
+    }
+
+    cout << "\n   Day             Amount Earned" << endl;
+   
+    cout << setprecision(2) << fixed;
+
+    for(int i = 0; i < num_of_days; i++)
+    {
+        total += penny;
+        cout << "   " << (i + 1) << "           $";
+        cout << penny << endl;
+        penny *= INCREMENTAL;
+    }
+
+    cout << "\nTotal salary for " << num_of_days;
+    cout << " days = $" << total << endl;*/
+
+/*const int MINIMUM = 0, MAXIMUM = 10;
+ char symbol = '+';
+
+    for(int i = MINIMUM; i < MAXIMUM; i++)
+    {
+        for(int j = MINIMUM; j <= i; j++)
+        {
+            cout << symbol;
+        }
+        cout << endl;
+    }
+cout<< endl;
+   
+    for(int i = MINIMUM; i < MAXIMUM; i++)
+    {
+        for(int j = MAXIMUM; j > i; j--)
+        {
+            cout << symbol;
+        }
+        cout << endl;
+    }*/
+
+  
+  
+  
+  
+
+
+  
+
+
+   }
